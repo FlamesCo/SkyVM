428fd89c9ce94e01d4353fb6e8753a084b2931cd
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2021-11-02 11:45:01 -0700

    SkyVM Client

---
+++ SkyVM.cs
@@ -0,0 +1,48 @@
+## make a virtual machhine client based off of the code from vmware
+ public class SkyVM
+ {
+## write a class that can be used to create a virtual machine
+public class {
+    public class Progran Program
+    {
+        public static void Main(string[] args)
+
+        {
+            Console.WriteLine("Booting SkyVM");
+            ## write a gui
+            
+        }
+         public class VMGI  [
+             ## write a gui
+             using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace program{
+    class program{
+        private static void Main(){
+            Form mainForm = new Form();
+            Label lblFirst = new Label();
+            mainForm.Width = 300;
+            mainForm.Height = 400; 
+            lblFirst.Text = "SkyClient v1.0a";
+            lblFirst.Location = new Point(150,200);
+            mainForm.Controls.Add(lblFirst);
+            Application.Run(mainForm);
+
+            ## write a gui for a main menu
+        mainform.menuStrip1.Items.Add("File");
+        mainform.menuStrip1.Items.Add("Edit");
+        mainform.menuStrip1.Items.Add("Help");
+        mainform.menuStrip1.Items.Add("View");
+        mainform.menuStrip1.Items.Add("Tools");
+        mainform.menuStrip1.Items.Add("Window");
+        mainform.menuStrip1.Items.Add("Run");
+        mainform.menuStrip1.Items.Add("Discord");
+
+        }   
+    }
+}
+
+         ]
+        {
